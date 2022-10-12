4e17a689d51ed3e910ccf67be347f60bab013cee
Neko1313 <91068060+Neko1313@users.noreply.github.com>
2022-10-12 15:58:26 +1000

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        dots();
+    }
+
+    private static void dots() {
+        FormDots f = new FormDots();
+        new Thread(f).start();
+    }
+
+}
